b482ae685318e88bd1c8bb18d4efc572cf5a0cb2
Anshul Thakur <43693743+imanshul@users.noreply.github.com>
2020-07-14 14:41:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 
 var os = require('os');
 var nodeStatic = require('node-static');
-var http = require('http');
+var https = require('https');
 var socketIO = require('socket.io');
 
+const port = process.env.PORT || 3000;
+
 var fileServer = new(nodeStatic.Server)();
-var app = http.createServer(function(req, res) {
+var app = https.createServer(function(req, res) {
   fileServer.serve(req, res);
-}).listen(8080);
+}).listen(port,'0.0.0.0');
 
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
